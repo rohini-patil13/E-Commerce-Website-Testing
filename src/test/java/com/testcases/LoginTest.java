be4954ed8abe300a62d4984d84e9dff9cc5732d4
rohini-patil13 <rohinidpatil13@gmail.com>
2024-10-06 15:06:30 +0530

    loginTest file updated

---
+++ src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,62 @@
+package com.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.base.BaseClass;
+import com.pageobjects.HomePage;
+import com.pageobjects.LoginSignupPage;
+
+public class LoginTest extends BaseClass{
+	
+	HomePage home = null;
+	LoginSignupPage login;
+	
+	@BeforeMethod
+	public void setup() {
+		loadConfig();
+		launchBrowser();
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void loginWithExistingUser() {
+		home = new HomePage();
+		home.isHomePageLoaded();
+		login = home.clickOnSignUpLoginInBtn();
+		login.isLoginTitleVisible();
+		login.enterLoginCreds("useracc@gmail.com", "Qwerty@123");
+		login.clickOnLoginBtn();
+		home.isUserLoggedIn("newUser");
+	}
+
+	@Test
+	public void loginwithInvalidCreds() {
+		home = new HomePage();
+		home.isHomePageLoaded();
+		login = home.clickOnSignUpLoginInBtn();
+		login.isLoginTitleVisible();
+		login.enterLoginCreds("fakeuser@gmail.com", "Qwerty@123");
+		login.clickOnLoginBtn();
+		login.isInvaidUserErrorDisplayed();
+	}
+	
+	@Test
+	public void logoutUser() {
+		home = new HomePage();
+		home.isHomePageLoaded();
+		login = home.clickOnSignUpLoginInBtn();
+		login.isLoginTitleVisible();
+		login.enterLoginCreds("useracc@gmail.com", "Qwerty@123");
+		login.clickOnLoginBtn();
+		home.isUserLoggedIn("newUser");
+		home.clickOnLogoutBtn();
+		login.isLoginTitleVisible();
+
+	}
+}
